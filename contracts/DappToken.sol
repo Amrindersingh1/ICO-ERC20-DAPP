e9b2260e824d2b9329267db86dbe94349d373f64
I338248 <amrinder.singh@sap.com>
2018-11-24 21:23:59 +0530

    3. ERC20 complete token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -8,6 +8,12 @@ contract DappToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     //Name
     string public name = "DApp Token";
 
@@ -18,6 +24,9 @@ contract DappToken {
     string public standard = "DApp Token v1.0";
 
     mapping(address => uint256) public balanceOf;
+    
+    //allowance
+    mapping(address => mapping(address => uint256)) public allowance;
 
     //constructor
     //Set Total number of Token
@@ -29,7 +38,7 @@ contract DappToken {
         totalSupply = _initialSupply;
     }
 
-    //Transfer
+    // Transfer
     function transfer(address _to, uint256 _value) public returns (bool success) {
        
         //Exception if account doesnt have enough
@@ -44,6 +53,37 @@ contract DappToken {
 
         return true;
 
+    }
+
+    // approve
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        
+        allowance[msg.sender][_spender] = _value;
+        
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+
+        // require from has enough
+        require(balanceOf[_from] >= _value);
+
+        //require allowance is big enough
+        require(allowance[_from][msg.sender] >= _value);
+
+        //change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        //update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        //Transfer Event
+        emit Transfer(_from, _to, _value);
+
+        return true;
     }
 
 
