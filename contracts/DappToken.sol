ef6b38c14d3a1c65a12c5f8e5787698b7353aca7
I338248 <amrinder.singh@sap.com>
2018-11-24 19:19:34 +0530

    1. Setup Token Balance with Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.2;
+
+contract DappToken {
+    //constructor
+
+    //Set Total number of Token
+
+    //Read Total number of Token
+    uint256 public totalSupply;
+    
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
